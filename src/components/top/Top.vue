c4523d7a696dd7b50f9b0593b888c969bef5640f
aquilaneo <aquilaneo@gmail.com>
2023-06-20 22:52:35 +0900

    スタイル修正

--- src/components/top/Top.vue
+++ src/components/top/Top.vue
@@ -41,15 +41,15 @@ const currentAudios: CurrentFileItem[] = [
 			<div>
 				<h2>最近開いたファイル</h2>
 			</div>
-			<div>
+			<div class="current-file-list__item">
 				<h3>イメージ</h3>
 				<CurrentFileList :items="currentImages"/>
 			</div>
-			<div>
+			<div class="current-file-list__item">
 				<h3>ビデオ</h3>
 				<CurrentFileList :items="currentVideos"/>
 			</div>
-			<div>
+			<div class="current-file-list__item">
 				<h3>オーディオ</h3>
 				<CurrentFileList :items="currentAudios"/>
 			</div>
@@ -65,7 +65,7 @@ const currentAudios: CurrentFileItem[] = [
 .top {
 	width: 100%;
 	height: 100%;
-	padding: 1rem;
+	padding: 1rem 0;
 
 	background-color: #FAFAFA;
 
@@ -76,10 +76,44 @@ const currentAudios: CurrentFileItem[] = [
 .top__panel {
 	width: 50%;
 	height: 100%;
+	padding: 0 1rem;
+}
+
+.top__panel:not(:last-child) {
+	border-right: solid var(--main-color) thin;
+}
+
+.top__panel--left {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+}
+
+.title {
+	width: 100%;
+	text-align: center;
+	margin-bottom: 4rem;
+}
+
+.open-file {
+	width: 100%;
+	text-align: center;
+}
+
+.open-file__button {
+	width: 8rem;
+	height: 8rem;
+	margin: 0 auto 1rem auto;
+	border: solid var(--main-color) thin;
+	border-radius: 0.4rem;
 }
 
 .top__panel--right {
 	display: grid;
 	grid-template-rows: 2rem 1fr 1fr 1fr 2rem;
+}
+
+.current-file-list__item:not(:last-child) {
+	border-bottom: solid var(--main-color) thin;
 }
 </style>
